92d22b5b1c5958c0211eabebd43e33b3dc4817be
Abdo Essam <92235360+abdo-essam@users.noreply.github.com>
2021-10-10 22:31:37 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Skip_list.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Skip_list.dir/main.cpp.obj"
+  "Skip_list.exe"
+  "Skip_list.exe.manifest"
+  "Skip_list.pdb"
+  "libSkip_list.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Skip_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
